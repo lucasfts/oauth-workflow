b27a49e759630b93b445788f7646b480b15b3377
Lucas Silva <luckkass89@gmail.com>
2022-06-19 12:49:33 -0300

    Token creation logic

---
+++ OAuthWorkflow/AuthorizationServer/Controllers/OAuthController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace AuthorizationServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OAuthController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<string> Authorize()
+        {
+            var privateKey = System.IO.File.ReadAllText("private-key.pem");
+            var rsa = RSA.Create();
+            rsa.ImportFromPem(privateKey.ToCharArray());
+            var securityKey = new RsaSecurityKey(rsa);
+            var claims = new Claim[] { new Claim(ClaimTypes.Name, "User123456") };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(1),
+                Issuer = "AuthorizationServer",
+                Audience = "ResourceServer",
+                SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.RsaSha512)
+            };
+
+            var token = new JwtSecurityTokenHandler().CreateToken(tokenDescriptor);
+
+            return Ok(token);
+        }
+    }
+}
